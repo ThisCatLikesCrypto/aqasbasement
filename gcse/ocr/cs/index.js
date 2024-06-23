f717955c01f0a4e4930b3590534159bdf6fb6a7f
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-06-23 20:05:10 +0100

    add basic ocr cs gcse page

---
+++ gcse/ocr/cs/index.js
@@ -0,0 +1,17 @@
+function Main(){
+    return html`
+    <div>
+        <h1>Computer Science Resources</h1>
+        <h2>OCR GCSE</h2>
+        <p>
+            <button><a href="https://www.ocr.org.uk/Images/558027-specification-gcse-computer-science-j277.pdf">Specification</a></button> 
+            <button><a href="https://www.savemyexams.com/gcse/computer-science/ocr/22/">SaveMyExams</a></button>
+        </p>
+        <p>This might fill up with my notes if I can be bothered</p>
+    </div>
+    `
+}
+
+window.addEventListener('load', function(){
+    document.getElementById("main").appendChild(h(Main));
+});
