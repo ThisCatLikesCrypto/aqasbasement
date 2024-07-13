04889493346981a0d65212e0d22ed44e40492e11
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-07-13 17:06:44 +0100

    completed CS 1.5 and fixed 404 i think

--- gcse/ocr/cs/index.js
+++ gcse/ocr/cs/index.js
@@ -18,6 +18,8 @@ function Main(){
         <a href="1/2_3.html">1.2 Part 3</a>
         <br>
         <a href="1/3_1.html">1.3 Part 1</a>
+        <br>
+        <a href="1/5.html">1.5</a>
     </div>
     `
 }
