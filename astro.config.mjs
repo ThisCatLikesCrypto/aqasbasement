1516378e051c72234cfae17b66c5a63d12ff70a9
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2025-09-02 19:40:04 +0100

    add cf adapter and re-bump

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://aqasbasement.uk'
+  site: 'https://aqasbasement.uk',
+  adapter: cloudflare()
 });
