39503cb53d1496cad157f7a5c008d60d033c08e2
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2025-09-02 19:42:55 +0100

    turns out i had to change the build system (remove adapter)
    in fairness, this wasnt explained in the page astro literally linked for me in the build log

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aqasbasement.uk',
-  adapter: cloudflare()
 });
